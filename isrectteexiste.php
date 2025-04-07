4ca3d4ceae1577cf61d90017b1ce933fa20ed715
aidanBarouk <aidan.barouk@etu-upsaclay.fr>
2025-04-07 13:55:06 +0200

    Delete isrectteexiste.php

--- isrectteexiste.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-  $jsonString = file_get_contents('recettes.json');
-  $data = json_decode($jsonString, true);
-  echo json_encode(array_search($_GET["recette"], array_column($data, 'name')));
-?>
