0790f476742f4811c9415a176512dbe2924e0e60
aidanBarouk <aidan.barouk@etu-upsaclay.fr>
2025-05-04 23:16:40 +0200

    partie_quasiFinale

--- donnees/projet.php
+++ donnees/projet.php
@@ -9,9 +9,14 @@ require_once 'check_session.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <title>Projet Javier Peña et Aidan Barouk</title>
-    <script>
+    <link rel="stylesheet" href="color.css">
 
+    <title>Projet Javier Peña et Aidan Barouk</title>
+    
+    
+    <script>
+        // ICI on trouve toutes les fonctions ajax qui servent au differentes fonctionnalités du site
+/*La fonction permet de changer le role de la personne en question*/ 
 function changerRole(email, username, newRole) {
     $.ajax({
         url: 'isRolechange.php',
@@ -38,7 +43,7 @@ function changerRole(email, username, newRole) {
     });
 }
 
-
+    /*Cette fonction affiche les utilisateurs qui demandent à etre changés de role*/
     function ListUtilisateur() {
         $.ajax({
             url: 'ListUtilisateur.php',
@@ -88,7 +93,7 @@ function changerRole(email, username, newRole) {
         });
     }
 
-
+    /*Cette fonction ajoute un commentaire*/
     function Commentaire(button){
 
         commentaire = $(button).closest('.zone-commentaire').find('.champ-commentaire').val();
@@ -118,7 +123,7 @@ function changerRole(email, username, newRole) {
         });
     }
 
-
+    /*La fonction va permettre de like ou delike une recette*/
     function Fav(button){
         const fav = $(button).data("name");
         const isLiked = $(button).hasClass("liked");
@@ -149,7 +154,7 @@ function changerRole(email, username, newRole) {
         }
     }
 
-
+    /*Cette fonction permet de verifier qu'une recette qu'on souhaite ajouter n'existe pas deja*/
     function submit() {
         console.log($('#name').val())
         $.ajax({
@@ -172,7 +177,7 @@ function changerRole(email, username, newRole) {
         $("#message").html("<span class='ko'> Error: network problem </span>");
       });
     }
-
+    /*Cette fonction va cherhcer la recette qu'on cherhce à traduire */
     function submitTR() {
         
         $.ajax({
@@ -199,7 +204,8 @@ function changerRole(email, username, newRole) {
         $("#message").html("<span class='ko'> Error: network problem </span>");
         });
     }
-    
+
+    /*Cette fonction va envoyer eune nouvelle recette dans le fichier json  */
     function sauvegarder() {
         const ingredients = [];
         $('#ingredientsContainer div').each(function() {
@@ -237,6 +243,7 @@ function changerRole(email, username, newRole) {
         });
     }
 
+    /*Cette fonction va modifier els traduction de la recette en question */
     function sauvegarderTR() {
         const recipeName = $("#recipeNameDisplay").text();
         const ingredientsFR = [];
@@ -306,7 +313,7 @@ function changerRole(email, username, newRole) {
             }
         });
     }
-
+       
     function login() {
         console.log($('#uname').val())
         $.ajax({
@@ -323,8 +330,8 @@ function changerRole(email, username, newRole) {
       });
     }
 
-    function signin() {
     
+    function signin() {
         const data = {
             email: $('#email').val(),
             username: $('#username').val(),
@@ -345,7 +352,7 @@ function changerRole(email, username, newRole) {
         $("#message").html("<span class='ko'> Error: network problem </span>");
       });
     }
-
+    /*Cette fonction va permettre la deconnexion */
     function logout() {
         $.ajax({
             method: "POST",
@@ -354,7 +361,7 @@ function changerRole(email, username, newRole) {
             location.reload();
         });
     }
-
+    /* Cette fonction va permettre de nous inscrire en ajoutant les données dans le fichier json user */
     function Continuer() {
     const data = {
         email: $('#email1').val(),
@@ -381,7 +388,7 @@ function changerRole(email, username, newRole) {
         $("#message").html("<span class='ko'> Error: network problem </span>");
     });
 }
-
+    /*Cette fonction va nous permettre de nous connecter pour avoir acces au different role ou pas */
     function connection() {
         console.log("Bouton Login cliqué"); // Debug 1
 
@@ -425,145 +432,172 @@ function changerRole(email, username, newRole) {
     </script>
 
   </head>
+
   <body>
-
-  <div id="dashboard" style="display:<?php echo isLoggedIn() ? 'block' : 'none'; ?>">
-    <h2>Bienvenue, <?php echo htmlspecialchars($_SESSION['user']['username'] ?? 'Invité'); ?></h2>
-    <p>Email: <?php echo htmlspecialchars($_SESSION['user']['email'] ?? ''); ?></p>
-    <p>Role: <?php echo implode(', ', $_SESSION['user']['role'] ?? []); ?></p>
     
-    <button onclick="logout()">Déconnexion</button>
-</div>
+     
 
+      <!--- Nous avons mis un header pour grouper le titre la connexion et l'inscription, ce header nous est surtout utilise pour le css-->
+  <header>
+         <h1 style="display: flex; align-items: center;">
+            <img src="oogway-removebg-preview.png" alt="Logo" style="height: 45px; margin-right: 10px;">
+             Oogway
+        </h1>
+        <div class="boutons-connexion">
 
-<div id="namerecette" style="display:<?php echo (isLoggedIn() && in_array('Chef', $_SESSION['user']['role'] ?? [])) ? 'block' : 'none'; ?>">
-    <h1>Ajouter une recette</h1>
-    <label for="name">Recipe Name:</label>
-    <input type="text" id="name" name="name" required><br><br>
-    <input type="hidden" name="fullForm" value="1">
-    <button onclick="submit()" >Continuer</button>
-    <p id="feedbackname">  </p>
-    </div>
-    
-    
-    <div id="formulaire" style="display:none">
-    <label for="nameFR">Nom recette (FR):</label>
-    <input type="text" id="nameFR" name="nameFR"><br><br>
-    
-    <label for="Author">Auteur:</label>
-    <input type="text" id="Author" name="Author"><br><br>
-    <label>Sans:</label><br>
-    <input type="checkbox" id="noGluten" name="Without[]" value="NoGluten">
-    <label for="noGluten">Sans Gluten</label><br>
-    <input type="checkbox" id="noMilk" name="Without[]" value="NoMilk">
-    <label for="noMilk">Sans Lait</label><br>
-    <input type="checkbox" id="vegan" name="Without[]" value="Vegan">
-    <label for="vegan">Vegan</label><br>
-    <input type="checkbox" id="vegetarien" name="Without[]" value="Vegetarien">
-    <label for="vegetarien">Végétarien</label><br><br>
-    <h3>Ingrédients:</h3>
-    <div id="ingredientsContainer"></div>
-    <button type="button" onclick="addIngredient()">+ Ajouter un ingrédient</button><br><br>
-    <label for="steps">Étapes (séparez chaque étape par un retour à la ligne):</label><br>
-    <textarea id="steps" name="steps" rows="5" cols="40"></textarea><br><br>
-    <label for="timers">Timers (en minutes, séparez-les par des virgules):</label>
-    <input type="text" id="timers" name="timers"><br><br>
+        <div id="signinn" style="display:<?php echo !isLoggedIn() ? 'block' : 'none'; ?>" >
+        <button onclick="signin()" >Sign in</button>
+        <p id="hola"></p>
+        </div>
 
-    <label for="imageURL">Lien Image:</label>
-    <input type="text" id="imageURL" name="imageURL"><br><br>
-    <label for="originalURL">Lien Original:</label>
-    <input type="text" id="originalURL" name="originalURL"><br><br>
+        <div id="loginn" style="display:<?php echo !isLoggedIn() ? 'block' : 'none'; ?>">
+        <button id="loginButton" onclick="login()" >Log in</button>
+        <p id="hola1"></p>
+        </div>
 
-    <input type="hidden" name="completeRecipe" value="1">
-    <button onclick="sauvegarder()" >Sauvegarder</button>
-    <p id="recetteelements">  </p>
+        <div id="dashboard" style="display:<?php echo isLoggedIn() ? 'block' : 'none'; ?>">
+            <h2>Bienvenue, <?php echo htmlspecialchars($_SESSION['user']['username'] ?? 'Invité'); ?></h2>
+            <p>Role: <?php echo implode(', ', $_SESSION['user']['role'] ?? []); ?></p>
+            
+            <button onclick="logout()" id="logoutButton">Déconnexion</button>
+        </div>
 
-    </div>
-
-    <div id="Succes" style="display:none">
-    <label>Recette enregistrée avec succès !</label><br>
-    </div>
-
-    <div id="signinn" style="display:<?php echo !isLoggedIn() ? 'block' : 'none'; ?>" >
-    <button onclick="signin()" >Sign in</button>
-    <p id="hola"></p>
-    </div>
-
-    <div id="inscrire" style="display:none">
-    <label for="email">Email:</label>
-    <input type="text" id="email1" name="email" required="required"><br><br>
-    
-    <label for="username">Username:</label>
-    <input type="text" id="username" name="username" required="required"><br><br>
-
-    <label for="password">Mot de Passe:</label>
-    <input type="password" id="password1" name="password" required="required"><br><br>
-
-    <label>Role:</label><br>
-    <input type="checkbox" id="Cuisinier" name="Role" value="Cuisinier">
-    <label for="Cuisinier">Cuisinier</label><br>
-    <input type="checkbox" id="DemandeTraducteur" name="Role" value="DemandeTraducteur">
-    <label for="DemandeTraducteur">Demande Traducteur</label><br>
-    <input type="checkbox" id="DemandeChef" name="Role" value="DemandeChef">
-    <label for="DemandeChef">Demande Chef</label><br>
-
-    <button onclick="Continuer()">Continuer</button>
-
-    <p id="nouvcompte"></p>
-</div>
-
-    <div id="loginn" style="display:<?php echo !isLoggedIn() ? 'block' : 'none'; ?>">
-    <button onclick="login()" >Log in</button>
-    <p id="hola1"></p>
-    </div>
-
-    <div id="connecter" style="display:none">
+        <div id="Connexion" style="color: red; font-weight: bold;"></div>
+        <div id="connecter" style="display:none">
     <label for="email">Email:</label>
     <input type="text" id="email" name="email" required="required"> <br><br>
 
     <label for="password">Mot de Passe:</label>
     <input type="password" id="password" name="password" required="required"><br><br>
 
-    <button onclick="connection()" >Log In</button>
-    <p id="Connexion"></p>
+    <button id="loginButton" onclick="connection()">Log In</button>
+    </div>
 
     </div>
 
-    <div id="namerecette2" style="display:<?php echo (isLoggedIn() && in_array('Traducteur', $_SESSION['user']['role'] ?? [])) ? 'block' : 'none'; ?>">
-    <h1>Traduire une recette</h1>
-    <label for="nameTR">Recipe Name:</label>
-    <input type="text" id="nameTR" name="nameTR" required><br><br>
-    <input type="hidden" name="fullForm" value="1">
-    <button onclick="submitTR()" > Vérifier</button>
-    <p id="traduire">  </p>
+    <div id="inscrire" style="display:none">
+            <label for="email">Email:</label>
+            <input type="text" id="email1" name="email" required="required"><br><br>
+            
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required="required"><br><br>
+
+            <label for="password">Mot de Passe:</label>
+            <input type="password" id="password1" name="password" required="required"><br><br>
+
+            <label>Role:</label><br>
+            <input type="checkbox" id="Cuisinier" name="Role" value="Cuisinier">
+            <label for="Cuisinier">Cuisinier</label><br>
+            <input type="checkbox" id="DemandeTraducteur" name="Role" value="DemandeTraducteur">
+            <label for="DemandeTraducteur">Demande Traducteur</label><br>
+            <input type="checkbox" id="DemandeChef" name="Role" value="DemandeChef">
+            <label for="DemandeChef">Demande Chef</label><br>
+
+            <button onclick="Continuer()">Continuer</button>
+
+            <p id="nouvcompte"></p>
+        </div>
+    </header>   
+
+    <div class="images-container">
+    <!--Image de part et d'autre de la fenetre -->
+    <img src="Je-veux-une-recette-de-plat-familial-original.jpg" alt="Image gauche" class="decor-left">
+    <img src="arrangement-with-food-kitchen.jpg" alt="Image droite" class="decor-right">
     </div>
-    
-    <div id="formulaire2" style="display:none">
-    <h2>Traduction de la recette: <span id="recipeNameDisplay"></span></h2>
-    
-    <label for="nameFR">Nom français:</label>
-    <input type="text" id="nameFR_TR" name="nameFR"><br><br>
-    
-    <h3>Traduction des ingrédients:</h3>
-    <div id="ingredientsContainerTR"></div>
-    
-    <h3>Traduction des étapes:</h3>
-    <textarea id="stepsTR" name="steps" rows="5" cols="40"></textarea><br><br>
-    
-    <button onclick="sauvegarderTR()">Enregistrer la traduction</button>
-    <p id="recetteelements2"></p>
+
+    <!--  ici on affiche les ingredients/etapes necessaires pour pouvoir ajouter une recette -->
+    <div class="barre-recherche">
+        <div id="namerecette" style="display:<?php echo (isLoggedIn() && in_array('Chef', $_SESSION['user']['role'] ?? [])) ? 'block' : 'none'; ?>">
+            <h1>Ajouter une recette</h1>
+            <label for="name">Recipe Name:</label>
+            <input type="text" id="name" name="name" required><br><br>
+            <input type="hidden" name="fullForm" value="1">
+            <button onclick="submit()" >Continuer</button>
+            <p id="feedbackname">  </p>
+            </div>
+            
+            
+            <div id="formulaire" style="display:none">
+            <label for="nameFR">Nom recette (FR):</label>
+            <input type="text" id="nameFR" name="nameFR"><br><br>
+            
+            <label for="Author">Auteur:</label>
+            <input type="text" id="Author" name="Author"><br><br>
+            <label>Sans:</label><br>
+            <input type="checkbox" id="noGluten" name="Without[]" value="NoGluten">
+            <label for="noGluten">Sans Gluten</label><br>
+            <input type="checkbox" id="noMilk" name="Without[]" value="NoMilk">
+            <label for="noMilk">Sans Lait</label><br>
+            <input type="checkbox" id="vegan" name="Without[]" value="Vegan">
+            <label for="vegan">Vegan</label><br>
+            <input type="checkbox" id="vegetarien" name="Without[]" value="Vegetarien">
+            <label for="vegetarien">Végétarien</label><br><br>
+            <h3>Ingrédients:</h3>
+            <div id="ingredientsContainer"></div>
+            <button type="button" onclick="addIngredient()">+ Ajouter un ingrédient</button><br><br>
+            <label for="steps">Étapes (séparez chaque étape par un retour à la ligne):</label><br>
+            <textarea id="steps" name="steps" rows="5" cols="40"></textarea><br><br>
+            <label for="timers">Timers (en minutes, séparez-les par des virgules):</label>
+            <input type="text" id="timers" name="timers"><br><br>
+
+            <label for="imageURL">Lien Image:</label>
+            <input type="text" id="imageURL" name="imageURL"><br><br>
+            <label for="originalURL">Lien Original:</label>
+            <input type="text" id="originalURL" name="originalURL"><br><br>
+
+            <input type="hidden" name="completeRecipe" value="1">
+            <button onclick="sauvegarder()" >Sauvegarder</button>
+            <p id="recetteelements">  </p>
+
+            </div>
+        </div>
+
+            <div id="Succes" style="display:none">
+            <label>Recette enregistrée avec succès !</label><br>
+            </div>
+
+      <!-- On va ici cherche la recette que l'on cherche à traduire -->
+    <div class="barre-recherche">
+        <div id="namerecette2" style="display:<?php echo (isLoggedIn() && in_array('Traducteur', $_SESSION['user']['role'] ?? [])) ? 'block' : 'none'; ?>">
+        <h1>Traduire une recette</h1>
+        <label for="nameTR">Recipe Name:</label>
+        <input type="text" id="nameTR" name="nameTR" required><br><br>
+        <input type="hidden" name="fullForm" value="1">
+        <button onclick="submitTR()" > Vérifier</button>
+        <p id="traduire">  </p>
+        </div>
+        
+        <div id="formulaire2" style="display:none">
+        <h2>Traduction de la recette: <span id="recipeNameDisplay"></span></h2>
+
+        
+        <label for="nameFR">Nom français:</label>
+        <input type="text" id="nameFR_TR" name="nameFR"><br><br>
+        
+        <h3>Traduction des ingrédients:</h3>
+        <div id="ingredientsContainerTR"></div>
+        
+        <h3>Traduction des étapes:</h3>
+        <textarea id="stepsTR" name="steps" rows="5" cols="40"></textarea><br><br>
+        
+        <button onclick="sauvegarderTR()">Enregistrer la traduction</button>
+        <p id="recetteelements2"></p>
+        </div>
+
+        <div id="Succes2" style="display:none">
+        <label>Traduction enregistrée avec succès !</label><br>
+        </div>
     </div>
 
-    <div id="Succes2" style="display:none">
-    <label>Traduction enregistrée avec succès !</label><br>
-    </div>
+  <!-- On cherche une recette  -->
+ <!-- Nous avons trouvé ce moyen de chercher sur internet, nous nous en sommes inspirer et adapté le code à notre requete -->
+<!--https://codemalin.fr/articles/creer-une-barre-de-recherche-javascript.html-->
 
-
-  
-
-    <h1>Rechercher une recette</h1>
+    <div class = "barre-recherche">
+    <h2>Rechercher une recette</h2>
     <input type="text" id="searchInput" class="search-bar" placeholder="Rechercher une recette...">
-    <div id="recipeList"></div>
+    </div>
+    <div id="recipeList" class="liste-recettes"></div>
     <div id="recipeDetails" class="recipe-details"></div>
 
     <script>
@@ -676,17 +710,21 @@ function changerRole(email, username, newRole) {
 
         
         </script>
+        <!-- Gestion des utilisateurs par l'admin -->
+
             <br><br>
+            <div class="barre-recherche">
             <div class="gestion-admin" style="display:<?php echo (isLoggedIn() && in_array('Directeur', $_SESSION['user']['role'] ?? [])) ? 'block' : 'none'; ?>">
                 <button  onclick="ListUtilisateur()">Liste des Utilisateurs Demandeurs</button>
             </div>
+            <br>
             <div id="userListModal" class="modal">
                 <div class="modal-body" id="userListContent">
                     <!-- Les données des utilisateurs seront affichées ici -->
                 </div>
             </div>
-
-            
+    </div>
+        
   </body>
 
 </html>
